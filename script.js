6a022a53060ade78f4d52d1000127ea0d4fc3943
Alvinjosepulickan <alvin.jose.007@gmail.com>
2023-01-01 22:13:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 'use strict';
-const model=document.querySelector('.modal');
-const overlay=document.querySelector('.overlay');
-const btnCloseModel=document.querySelector('.close-modal');
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
 //when multiple elements with same selector below method returen a list
-const btnshowModel=document.querySelectorAll('.show-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  };
-  
-  const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  };
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 //iterate through list in line no 6 and add evenet to each selector
-for(let i=0;i<btnshowModel.length;i++)
-{
-    btnshowModel[i].addEventListener('click', openModal);
-}
- document.addEventListener('keydown', function (e) {
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
   // console.log(e.key);
 
   if (e.key === 'a' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
+
+
+
+
+
